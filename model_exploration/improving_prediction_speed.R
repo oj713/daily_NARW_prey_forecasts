823f9fe9d574f94d8c330c82650eedabe960cec2
Omi Johnson <ojohnson@bigelow.org>
2025-08-18 16:39:15 -0400

    wrapped experiments on improving prediction speed

--- model_exploration/improving_prediction_speed.R
+++ model_exploration/improving_prediction_speed.R
@@ -1,85 +1,149 @@
+setwd("/mnt/ecocast/projects/students/ojohnson/daily-forecasts")
+print(getwd())
 species <- "jellyfish"
 source("setup.R")
 source("data_preparation/data_from_config.R")
 source("generate_prediction_cubes.R")
 
 v <- "je.0.00"
+config <- read_config(v)
 wkfs <- get_v_wkfs(v)
 n_folds <- length(wkfs)
 
-data <- generate_covariate_cube(read_config(v), as.Date(c("2015-07-14", "2015-07-16")))
+data <- generate_covariate_cube(config, as.Date(c("2015-07-14", "2015-07-16")))
 
-# Calculates predictions from a single workflow
-get_wkf_column <- function(wkf, idx) {
-  res <- predict(wkf, data, type = "prob") |>
-    select(.pred_1)
-  
-  if (TRUE) {
-    cat("\r Predicting...", idx, "/", n_folds)
+#' Calculates time taken to do a thing, average of three runs
+#' basically system.time with printing
+timeTaken <- function(functionToRun) {
+  oneIter <- function(n) {
+    cat("\r",  n, "/ 3 ...")
+    starttime <- Sys.time()
+    functionToRun()
+    as.numeric(Sys.time() - starttime)
   }
-  
-  res
+  1:3 |> sapply(oneIter) |> mean()
 }
-wkf_preds <- wkfs |>
-  imap(get_wkf_column) |>
-  bind_cols() |>
-  suppressMessages()
 
-
-
-apply_quantile_preds <- function(wkfs, data, 
-                                 desired_quants = c(0, .05, .5, .95, 1), 
-                                 verbose = FALSE, na.ignore = TRUE) {
-  n_folds <- length(wkfs)
-  predictable_indices <- (if (na.ignore) { complete.cases(data) }
-                          else {TRUE})
-  
-  if(verbose) {cat("\n Predicting... 0 /", n_folds)}
-  
+# No parallel, calling predictable subset off the bat
+# 30.876
+orig <- function() {
   # Calculates predictions from a single workflow
+  predictable_indices <- complete.cases(data)
+  
   get_wkf_column <- function(wkf, idx) {
     res <- predict(wkf, data[predictable_indices,], type = "prob") |>
       select(.pred_1)
     
-    if (verbose) {
+    if (TRUE) {
       cat("\r Predicting...", idx, "/", n_folds)
     }
     
     res
   }
+  
   wkf_preds <- wkfs |>
     imap(get_wkf_column) |>
     bind_cols() |>
     suppressMessages()
+}
+
+# Same as original, but partitioning data by predictableIndices one time
+# 30.425, little bit faster! 
+orig_oneSubset <- function() {
+  # Calculates predictions from a single workflow
+  predictable_indices <- complete.cases(data)
+  predictable_data <- data[predictable_indices,]
   
-  if(verbose) {cat("\r Calculating quantiles...")}
-  pred_quantiles <- NULL
-  # Different calculation methods for high fold versus fold count ≤ 3
-  if (n_folds >= 3) {
-    # Calculate quantiles
-    if (n_folds == 3) {desired_quants <- c(0, .5, 1)}
-    # matrixStats is 10x faster than apply(x, 1, quantile)
-    pred_quantiles <- wkf_preds |>
-      t() |> 
-      matrixStats::colQuantiles(probs = desired_quants)
+  get_wkf_column <- function(wkf, idx) {
+    res <- predict(wkf, predictable_data, type = "prob") |>
+      select(.pred_1)
     
-  } else if (n_folds <= 2) {
-    # Mean (n_folds = 2) OR identity (n_folds = 1)
-    desired_quants <- c(.5)
-    if (n_folds == 2) {
-      pred_quantiles <- rowMeans(wkf_preds) |>
-        as_tibble_col()
+    if (TRUE) {
+      cat("\r Predicting...", idx, "/", n_folds)
     }
-    colnames(pred_quantiles) <- paste0(desired_quants * 100, "%")
+    
+    res
   }
   
-  if(verbose) {cat("\r Formatting to return...")}
-  
-  # Adding back empty entries for NA values
-  returnable_data <- data |>
-    select(lon, lat, date)
-  returnable_data[, colnames(pred_quantiles)] <- NA
-  returnable_data[predictable_indices, colnames(pred_quantiles)] <- pred_quantiles
-  
-  returnable_data
+  wkf_preds <- wkfs |>
+    imap(get_wkf_column) |>
+    bind_cols() |>
+    suppressMessages()
 }
+
+# The recipe is the same for all models // extract and bake once
+# 29.8912 Litte faster! 
+oneS_splitoutRecipe <- function() {
+  wkf_rec <- extract_preprocessor(wkfs[[1]]) |> prep()
+  predictable_indices <- complete.cases(data)
+  id_vars <- wkf_rec$term_info |>
+    filter(role == "ID") |>
+    pull(variable)
+  predictable_data <- wkf_rec |>
+    bake(new_data = data[predictable_indices,]) |>
+    select(-all_of(id_vars))
+  
+  models <- map(wkfs, extract_fit_parsnip)
+  
+  get_mod_column <- function(mod, idx) {
+    res <- predict(mod, predictable_data, type = "prob") |>
+      select(.pred_1)
+    
+    if (TRUE) {
+      cat("\r Predicting...", idx, "/", n_folds)
+    }
+    
+    res
+  }
+  
+  wkf_preds <- models |>
+    imap(get_mod_column) |>
+    bind_cols() |>
+    suppressMessages()
+}
+
+# Add parallel processing
+library(furrr)
+
+preptime <- Sys.time()
+wkf_rec <- extract_preprocessor(wkfs[[1]]) |> prep()
+predictable_indices <- complete.cases(data)
+id_vars <- wkf_rec$term_info |>
+  filter(role == "ID") |>
+  pull(variable)
+predictable_data <- wkf_rec |>
+  bake(new_data = data[predictable_indices,]) |>
+  select(-all_of(id_vars))
+
+models <- map(wkfs, extract_fit_parsnip)
+
+preptime <- as.numeric(Sys.time() - preptime)
+
+## MULTISESSION : 10.236
+plan(multisession, workers = 4)
+
+cat("|", paste(rep(" ", n_folds), collapse = ""), "| Predicting...")
+
+oneS_splitoutRecipe <- function() {
+  get_mod_column <- function(mod, idx) {
+    cat("\r|", paste(rep("+", idx), collapse = ""))
+    predict(mod, predictable_data, type = "prob") |>
+      dplyr::select(.pred_1)
+  }
+  
+  wkf_preds <- models |>
+    furrr::future_imap(get_mod_column,
+                       .options = furrr_options(seed = config$model$seed)) |>
+    bind_cols() |>
+    suppressMessages()
+  
+  cat("\n")
+}
+
+oneS_splitoutRecipe()
+
+# res <- timeTaken(oneS_splitoutRecipe) + preptime
+# print(res)
+
+plan(sequential)
+
